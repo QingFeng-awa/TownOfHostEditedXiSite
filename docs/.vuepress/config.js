166eea4d64caab3784949844b0efdc5523f824da
QingFeng <qingfeng_awa@163.com>
2025-03-14 17:27:03 +0800

    Update Config
    Update Nav and Notice

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,42 +3,28 @@ import { defineUserConfig } from 'vuepress/cli'
 import { viteBundler } from '@vuepress/bundler-vite'
 import { noticePlugin } from '@vuepress/plugin-notice'
 
-export default defineUserConfig({
-  lang: 'zh-CN',
-  head: [['link', { rel: 'shortcut icon', href: '/favicon.png' }]],
-  title: 'TOHEX官方网站',
-  description: '为继承TOHE而衍生的分支',
-  plugins: [
-    noticePlugin({
-      config: [
-        {
-          path: "/",
-          title: "TOHEX官网回归!",
-          content: "TOHEX自2024年8月23日起将开始稳定运行并提供完整功能。<br>TOHEX官方网站将持续运营到域名到期(2025.9.10)。",
-          showOnce: true,
-          fullscreen: true
-        }
-      ]
-    })
-  ],
+export 默认 defineUserConfig({
+  lang: 'zh-CN'，
+  head: [['link'， { rel: 'shortcut icon'， href: '/favicon.png' }]]，
+  title: 'TOHEX官方网站'，
+  description: '为继承TOHE而衍生的分支'，
   theme: defaultTheme({
-    lastUpdated: false,
-    contributors: false,
-    editlink: false,
-    logo: '/TOHEX.png',
+    lastUpdated: false，
+    贡献者: false，
+    editlink: false，
+    logo: '/TOHEX.png'，
     navbar: [
-      { text: "首页", link: "/"},
-      { text: "关于", link: "/about"},
-      { text: "下载", link: "/download"}
-    ],
+      { text: "首页"， link: "/"}，
+      { text: "关于"， link: "/about"}
+    ]，
     locales: {
       "/": {
-        notFound: ["404NotFound - 页面未找到"],
-        backToHome: "返回首页",
-        toggleColorMode: "切换颜色模式",
+        notFound: ["404NotFound - 页面未找到"]，
+        backToHome: "返回首页"，
+        toggleColorMode: "切换颜色模式"，
         toggleSidebar: "打开导航栏"
       }
     }
-  }),
-  bundler: viteBundler(),
+  })，
+  bundler: viteBundler()，
 })
